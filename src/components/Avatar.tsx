bab24685b409c2b41ddbe97c177a8df6f788b5cb
Kodai Hagihara <39426616+EringiV3@users.noreply.github.com>
2021-12-21 22:02:21 +0900

    fix: Avatar component bug

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,11 +1,16 @@
 import { Avatar as ChakraAvatar, Box } from '@chakra-ui/react';
 import NextImage from 'next/image';
+import { NEXT_IMAGE_DOMAINS } from '../config/constants';
 
 type Props = {
   size: number;
   src?: string;
 };
 const Avatar: React.FC<Props> = ({ size, src }) => {
+  const srcUrl = src ? new URL(src) : undefined;
+  const useNextImage = srcUrl
+    ? NEXT_IMAGE_DOMAINS.includes(srcUrl.hostname)
+    : false;
   return (
     <Box
       borderRadius="50%"
@@ -13,8 +18,10 @@ const Avatar: React.FC<Props> = ({ size, src }) => {
       height={`${size}px`}
       overflow="hidden"
     >
-      {src ? (
+      {useNextImage && src ? (
         <NextImage src={src} width={size} height={size} />
+      ) : src ? (
+        <ChakraAvatar src={src} width={size} height={size} />
       ) : (
         <ChakraAvatar width={size} height={size} />
       )}
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -5,6 +5,8 @@ export const AUTH0_AUDIENCE = process.env.AUTH0_AUDIENCE ?? '';
 export const GRAPHQL_END_POINT = process.env.GRAPHQL_END_POINT ?? '';
 export const MEDIA_HOST = process.env.MEDIA_HOST ?? '';
 export const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID ?? '';
+export const NEXT_IMAGE_DOMAINS =
+  process.env.NEXT_IMAGE_DOMAINS?.split(',') ?? [];
 
 export const TWITTER_URL = 'https://twitter.com';
 export const INSTAGRAM_URL = 'https://www.instagram.com';
