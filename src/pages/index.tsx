79e27be3ac0a8eaa0c99e0f111ffc69eb4cff9d3
Kodai Hagihara <39426616+EringiV3@users.noreply.github.com>
2021-12-26 20:34:45 +0900

    impl: lp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,76 @@
 import { useAuth0 } from '@auth0/auth0-react';
+import { Box } from '@chakra-ui/react';
+import NextImage from 'next/image';
 import React from 'react';
+import Button from '../components/Button';
 import Layout from '../components/Layout';
-import LoginButton from '../components/LoginButton';
+import { COLOR_CODE_INDIGO_BLUE } from '../config/css';
 
-const Profile = () => {
-  const { user, isAuthenticated } = useAuth0();
+const IndexPage = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  const handleClickButton = () => {
+    loginWithRedirect();
+  };
 
   return (
     <Layout>
-      {isAuthenticated && user ? (
-        <div>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src={user.picture} alt={user.name} />
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
-        </div>
-      ) : (
-        <div>
-          <LoginButton />
-        </div>
-      )}
+      <Box color={COLOR_CODE_INDIGO_BLUE} marginTop="40px">
+        <Box>
+          <Box textAlign="center" fontSize="4xl" fontWeight="bold">
+            ジーンズの色落ちを記録・共有できる
+          </Box>
+          <Box textAlign="center" fontSize="6xl" fontWeight="bold">
+            Denitz
+          </Box>
+          <Box display="flex" justifyContent="center" marginTop="40px">
+            <Button onClick={handleClickButton} width="50%">
+              新規登録
+            </Button>
+          </Box>
+          <Box display="flex" justifyContent="center" marginTop="40px">
+            <NextImage
+              src="/assets/lp_top.jpg"
+              width="500"
+              height="800"
+              objectFit="contain"
+            />
+          </Box>
+        </Box>
+        <Box>
+          <Box
+            textAlign="center"
+            fontSize="xl"
+            fontWeight="bold"
+            marginTop="40px"
+          >
+            使い方
+          </Box>
+          <Box>
+            <Box size="lg" marginTop="20px">
+              1. 自分の持っているデニムを追加します
+            </Box>
+          </Box>
+          <Box>
+            <Box size="lg" marginTop="20px">
+              2. デニムの色落ちを定期的に記録して投稿！
+            </Box>
+          </Box>
+        </Box>
+        <Box>
+          <Box
+            textAlign="center"
+            fontSize="xl"
+            fontWeight="bold"
+            marginTop="40px"
+          >
+            最新色落ち記録投稿
+          </Box>
+          <Box>色落ち記録新着５件</Box>
+        </Box>
+      </Box>
     </Layout>
   );
 };
 
-export default Profile;
+export default IndexPage;
